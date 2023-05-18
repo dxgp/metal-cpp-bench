a066fc4148634e4beabfe1c63ff9b92b9dfd8964
dxgp_ <gunjanpayal@icloud.com>
2023-05-18 15:44:34 +0530

    mmatmul

--- matmul/main.cpp
+++ matmul/main.cpp
@@ -5,6 +5,8 @@
 int main(){
     MTL::Device* device = MTL::CreateSystemDefaultDevice();
     MetalMatmul* met = new MetalMatmul(device, 4);
+    
     met->sendComputeCommand();
+    
     return 0;
 }
